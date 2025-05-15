aab467dc1dd98d949381140324881d31d3f41c85
Zain Mansoor <zainmansoor77@gmail.com>
2025-05-15 16:26:36 +0200

    Algebric equation solving

---
+++ Algebric_equation_solving.m
@@ -0,0 +1,28 @@
+clc;
+
+%equation = [1 4];
+%roots(equation)
+
+syms x
+equation = x + 4 == 0;
+sol = solve([equation],[x]);
+fprintf('sol = %d \n',sol);
+%disp(sol);
+
+equation = [1 0 4];
+roots(equation)
+
+equation = [1 0 -4];
+roots(equation)
+
+equation = [2 1 0 4];
+roots(equation)
+
+syms x y;
+eq1 = 2*x + 8*y == 15;
+eq2 = 9*x - 6*y == 21;
+sol = solve([eq1,eq2],[x,y]);
+fprintf('sol x = ')
+disp(sol.x);
+fprintf('\nsol y = ')
+disp(sol.y);
