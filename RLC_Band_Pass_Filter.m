4e3d1890763ecb16e0a329f26dc10a8b025bfc2e
Zain Mansoor <zainmansoor77@gmail.com>
2025-07-28 19:03:46 +0200

    RLC Band pass filter

--- RLC_Band_Pass_Filter.m
+++ RLC_Band_Pass_Filter.m
@@ -16,13 +16,19 @@ LC = 1 / ((2 * pi * f0)^2);
 
 R = 50;
 L = R / (B * 2 * pi);
+fprintf('L=%.4fu\n',L * 1e6);
 C = 1 / ((2 * pi * f0)^2 * L);
+fprintf('C=%.4fu\n',C * 1e6);
 R = B * 2 * pi * L; % R calculated with the help of Bandwidth
+fprintf('R=%.4f\n',R);
 %R = sqrt(L / ((Q^2) * C)); % R calculated with the help of Quality factor Q
 Q = 2 * pi * f0 * L / R;
+fprintf('Q=%.4f\n',Q);
 
 f1 = (1 / (2 * pi)) * [(-R / (2 *L)) + sqrt((R / (2 * L))^2 + (1 /(L * C)))];
 f2 = (1 / (2 * pi)) * [(R / (2 *L)) + sqrt((R / (2 * L))^2 + (1 /(L * C)))];
+fprintf('f1=%.4f\n',f1);
+fprintf('f2=%.4f\n',f2);
 
 B = f2 - f1;
 
